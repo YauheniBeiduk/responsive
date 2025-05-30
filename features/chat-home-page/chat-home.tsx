0b721540dee99b54c170931f732d4e29be24338c
YauheniBeiduk <Yauheni_Beiduk@epam.com>
2025-05-30 12:07:49 +0400

    add responsive

--- features/chat-home-page/chat-home.tsx
+++ features/chat-home-page/chat-home.tsx
@@ -11,24 +11,26 @@ export const ChatHome = () => {
   const t = useTranslations('Chat');
   return (
     <ScrollArea className="flex-1">
-      <main className="flex flex-1 flex-col items-center justify-center gap-6 pt-40">
-        <div className="flex flex-col items-center justify-center">
-          <Image src={'/mega_logo.png'} width={200} height={100} alt="logo"></Image>
-        </div>
-        <ChatMessageArea
-          key={'0'}
-          messageId={'0'}
-          profileName={'MegaGpt'}
-          role={'system'}
-          onCopy={() => {
-            navigator.clipboard.writeText('Hello data');
-          }}
-          profilePicture={'/logo.png'}
-        >
-          <MessageContent message={{ content: t('helpQuestion'), role: 'user', name: 'Mega' }} />
-        </ChatMessageArea>
-      </main>
-      <ChatInput />
+      <div className="flex h-full h-screen flex-col">
+        <main className="flex flex-1 flex-col items-center justify-center gap-6 pt-40">
+          <div className="flex flex-col items-center justify-center">
+            <Image src={'/mega_logo.png'} width={200} height={100} alt="logo"></Image>
+          </div>
+          <ChatMessageArea
+            key={'0'}
+            messageId={'0'}
+            profileName={'MegaGpt'}
+            role={'system'}
+            onCopy={() => {
+              navigator.clipboard.writeText('Hello data');
+            }}
+            profilePicture={'/logo.png'}
+          >
+            <MessageContent message={{ content: t('helpQuestion'), role: 'user', name: 'Mega' }} />
+          </ChatMessageArea>
+        </main>
+        <ChatInput />
+      </div>
     </ScrollArea>
   );
 };
